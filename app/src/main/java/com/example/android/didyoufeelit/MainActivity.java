32ca25e74d070b9043e5c494679d28c1a6964993
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-02-05 12:34:03 +0530

    Fix crash by moving network request to AsyncTask

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,24 +35,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+        // Make network request to fetch the earthquake event data on a background thread
+        // using a {@link AsyncTask} and then update the UI on main thread
+        new FetchEarthquakeAsyncTask().execute();
+    }
 
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+    /**
+     * {@link AsyncTask} to perform network request on a background thread, and then
+     * update the UI with the first earthquake in response.
+     */
+    private class FetchEarthquakeAsyncTask extends AsyncTask<Void, Void, Event> {
+
+        @Override
+        protected Event doInBackground(Void... voids) {
+            // Perform the HTTP request for earthquake data and process the response.
+            return Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            // If event data is null then exit early
+            if (event == null) {
+                return;
+            }
+
+            // Update the information displayed to the user.
+            updateUi(event);
+        }
     }
 
     /**
      * Update the UI with the given earthquake information.
      */
     private void updateUi(Event earthquake) {
-        TextView titleTextView = (TextView) findViewById(R.id.title);
+        TextView titleTextView = findViewById(R.id.title);
         titleTextView.setText(earthquake.title);
 
-        TextView tsunamiTextView = (TextView) findViewById(R.id.number_of_people);
+        TextView tsunamiTextView = findViewById(R.id.number_of_people);
         tsunamiTextView.setText(getString(R.string.num_people_felt_it, earthquake.numOfPeople));
 
-        TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
+        TextView magnitudeTextView = findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
     }
 }
